1883b46be214eb4f863762c2faf940131bea61e7
Hwan <head1ton@gmail.com>
2025-08-08 00:27:37 +0900

    1 단계 test

---
+++ linkedin.py
@@ -0,0 +1,44 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
+    """scrape information from LinkedIn profiles,
+    Manually scrape the information from the LinkedIn profile"""
+
+    if mock:
+        linkedin_profile_url = "https://gist.githubusercontent.com/head1ton-Voyager/e8bc7a83a46d0e379380f919da421d8a/raw/8d742ef7305711e5050d8ef34bccaecd8d07c778/head1ton-scrapin.json"
+        response = requests.get(
+            linkedin_profile_url,
+            timeout=10,
+        )
+    else:
+        api_endpoint = "https://api.scrapin.io/enrichment/profile"
+        params = {
+            "apikey": os.environ["SCRAPIN_API_KEY"],
+            "linkedInUrl": linkedin_profile_url,
+        }
+        response = requests.get(
+            api_endpoint,
+            params=params,
+            timeout=10,
+        )
+
+    data = response.json().get("person")
+    data = {k: v for k, v in data.items() if
+            v not in ([], "", "", None) and k not in ["certifications"]}
+
+    return data
+
+
+if __name__ == "__main__":
+    print(
+        scrape_linkedin_profile(
+            linkedin_profile_url="https://www.linkedin.com/in/daehwancho/",
+            mock=True
+        )
+    )
