39c9979de0dd316e68ed0e7cf90c39bb91d8f425
Hwan <head1ton@gmail.com>
2025-08-09 00:31:36 +0900

    linked in profile

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -29,6 +29,8 @@ def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
         )
 
     data = response.json().get("person")
+    if not data:
+        return {}
     data = {k: v for k, v in data.items() if
             v not in ([], "", "", None) and k not in ["certifications"]}
 
