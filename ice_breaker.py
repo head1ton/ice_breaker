853e64e1782906885bceeac8fa2c01ca1d7ee1aa
Hwan <head1ton@gmail.com>
2025-08-09 13:03:20 +0900

    web ice_breaker

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 # from langchain_community.chat_models import ChatOllama
@@ -5,14 +7,14 @@ from langchain_openai import ChatOpenAI
 
 from agent.linkedin_lookup_agent import lookup as linkedin_look_agent
 from agent.twitter_lookup_agent import lookup as twitter_lookup_agent
-from output_parser import summary_parser
+from output_parser import summary_parser, Summary
 from third_parties.linkedin import scrape_linkedin_profile
 from third_parties.twitter import scrape_user_tweets_mock
 
 load_dotenv()
 
 
-def ice_break_with(name: str) -> str:
+def ice_break_with(name: str) -> Tuple[Summary, str]:
     linkedin_username = linkedin_look_agent(name=name)
     linkedin_data = scrape_linkedin_profile(
         linkedin_profile_url=linkedin_username)
@@ -40,11 +42,11 @@ def ice_break_with(name: str) -> str:
 
     llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
 
-    chain = summary_prompt_template | llm | summary_parser()
+    chain = summary_prompt_template | llm | summary_parser
 
-    res = chain.invoke(input={"information": linkedin_data, "twitter_posts": tweets})
+    res: Summary = chain.invoke(input={"information": linkedin_data, "twitter_posts": tweets})
 
-    print(res)
+    return res, linkedin_data.get("profile_pic_url")
 
 if __name__ == "__main__":
     print("Ice Breaker Enter")
